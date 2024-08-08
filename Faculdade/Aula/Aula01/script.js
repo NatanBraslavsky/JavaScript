9334c7af981665e56299653f498f0eb09d1dd78f
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-08 18:40:20 -0300

    aula da faculdade e do curso em video

---
+++ Faculdade/Aula/Aula01/script.js
@@ -0,0 +1,38 @@
+// alert('Bom dia! \nSejam Bem-Vindos!' + Date())
+// document.write('Bom dia!<br/>Sejam Bem-Vindos!', Date())
+// console.log('Bom dia\nSejam Bem-Vindos!', Date())
+
+
+
+// document.write('O resoltado é : ' + 12 + 45)
+// document.write('<br>O resultado é : ', 12,45)
+// document.write('<br>O resultado é : '+ (5+4))
+
+
+
+// const nome = "Thereza Gondim"
+// const idade = 15
+
+// document.write(`O nome da pessoa é ${nome} ela tem ${idade} anos.`)
+
+
+
+
+// var nome = prompt('Digite o seu nome: ')
+// var idade = prompt('Digite a sua idade: ')
+// const nomeCapitalizado = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
+// document.write(`Seu nome é ${nomeCapitalizado} e sua idade é de ${idade} anos`)
+
+
+
+
+
+valor1 = parseInt(prompt('Digite o valor 1'))
+valor2 = parseFloat(prompt('Digite o valor 2'))
+valor3 = Number(prompt('Digite o valor 3'))
+valor4 = +(prompt('Digite o valor 4'))
+
+soma = valor1 + valor2 + valor3 + valor4
+document.write('<br>A soma é = ' + soma)
+
+
