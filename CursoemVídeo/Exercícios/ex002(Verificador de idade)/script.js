6001ba691e6a30498f4888a4a77dcc96a73eb9b3
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-10 14:12:31 -0300

    término do exercício de verificador de idade

--- CursoemVídeo/Exercícios/ex002(Verificador de idade)/script.js
+++ CursoemVídeo/Exercícios/ex002(Verificador de idade)/script.js
@@ -1,22 +1,41 @@
-var agora = new Date()
-var ano = agora.getFullYear()
-var img = window.document.getElementById('img')
-
-function clique(){
-    var nasc = window.document.getElementById('txtnum').value
-    var resp = window.document.getElementById('result')
-    
-
-    var anoNasc = Number(nasc)
-    var idade = ano - anoNasc
-    resp.innerHTML = idade
-
-    
-    if(idade<=12){
-        img.src = 'criancahomem.jpg'
-    } else if(idade <=30){
-        img.src = 'adolhomem.jpg'
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('Verifique os dados e tente novamente!')
     } else {
-        img.src = 'idoso.jpeg'
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                // Criança
+                img.setAttribute('src', 'criancahomem.jpg')
+            } else if (idade < 30){
+                // Jovem
+                img.setAttribute('src', 'adolhomem.jpg')
+            } else {
+                img.setAttribute('src', 'idoso.jpeg')
+            }
+        } else if(fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                // Criança
+                img.setAttribute('src', 'criancamulher.jpg')
+            } else if (idade < 30){
+                // Jovem
+                img.setAttribute('src', 'adolmulher.jpeg')
+            } else {
+                img.setAttribute('src', 'idosamulher.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = (`${gênero} com ${idade} anos`)
+        res.appendChild(img)
     }
 }
