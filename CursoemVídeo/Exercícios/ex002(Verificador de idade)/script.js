855bca6a3d448028d0a566035bf4b5ce751dbd49
Natan Pereira Braslavsky de Oliveira <natanbraslavsky1@gmail.com>
2024-08-11 11:41:59 -0300

    Comentários para revisão de exercício

--- CursoemVídeo/Exercícios/ex002(Verificador de idade)/script.js
+++ CursoemVídeo/Exercícios/ex002(Verificador de idade)/script.js
@@ -1,15 +1,15 @@
-function verificar(){
+function verificar(){ 
     var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano){
+    var ano = data.getFullYear()//pegar o ano atual
+    var fano = document.getElementById('txtano')//pegar o ano digitado
+    var res = document.getElementById('res')//pegar a resposta
+    if (fano.value.length == 0 || fano.value > ano){//verificar se o ano nao for digitado ou maior que o ano atua
         window.alert('Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
+        var fsex = document.getElementsByName('radsex')//pegar o sexo escolhifo
+        var idade = ano - Number(fano.value)//calcular idade
         var gênero = ''
-        var img = document.createElement('img')
+        var img = document.createElement('img')//cria ums imagem
         img.setAttribute('id', 'foto')
         if(fsex[0].checked) {
             gênero = 'Homem'
