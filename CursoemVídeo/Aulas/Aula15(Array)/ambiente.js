ee6db565641565f3716db0ec352938d145dc0d84
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-13 18:10:13 -0300

    aula 15 do CEV

---
+++ CursoemVídeo/Aulas/Aula15(Array)/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if(pos != -1){
+    console.log(`O valor 8 está na posição ${pos}.`)
+} else {
+    console.log(`O valor 8 não foi encontrado no vetor.`)
+}
---
+++ CursoemVídeo/Aulas/Aula15(Array)/vetor.js
@@ -0,0 +1,16 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+// console.log(valores[0])
+// console.log(valores[1])
+// console.log(valores[2])
+// console.log(valores[3])
+// console.log(valores[4])
+// console.log(valores[5])
+
+// for(let i = 0; i < valores.length; i++){
+//     console.log(valores[i])
+// }
+
+for(let pos in valores){
+    console.log(valores[pos])
+}
