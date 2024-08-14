8d6a39835889211a0363fdae1fe8d721f4632478
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-13 22:25:08 -0300

    aula função js

--- CursoemVídeo/Aulas/Aula15(Array)/vetor.js
+++ CursoemVídeo/Aulas/Aula15(Array)/vetor.js
@@ -13,4 +13,4 @@ let valores = [8, 1, 7, 4, 2, 9]
 
 for(let pos in valores){
     console.log(valores[pos])
-}
+} 
---
+++ CursoemVídeo/Aulas/Aula16(Função)/funcao01.js
@@ -0,0 +1,10 @@
+function parimpar(n){
+    if(n % 2 == 0){
+        return 'Par!'
+    } else{
+        return 'Ímpar!'
+    }
+}
+
+let res = parimpar(3)
+console.log(res)
