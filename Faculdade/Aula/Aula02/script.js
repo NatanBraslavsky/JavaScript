5612ca03ea903f4105d698b7d54c6abc02993d91
Natan Pereira Braslavsky de Oliveira <natanbraslavsky1@gmail.com>
2024-08-15 11:25:27 -0300

    aula js

---
+++ Faculdade/Aula/Aula02/script.js
@@ -0,0 +1,13 @@
+var a = Number(prompt('Digite um número'))
+a += 5
+alert('A soma dos valores = ' + a)
+document.write('A soma dos valores = <br>' + a)
+console.log('A soma dos valores é '+ a)
+//---------------------------------------
+var b = Number(prompt('Digite um número'))
+b -= a
+alert(`A subtração dos valores = ${b}`)
+document.write(`A subtração de B e A é ${b}`)
+console.log(`A subtração dosvalores = ${b}`)
+//---------------------------------------
+    
