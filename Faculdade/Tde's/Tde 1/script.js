ea764a2bb4b3cb1a61d3ab4059ca4ed292f9ff6c
Natan Pereira Braslavsky de Oliveira <natanbraslavsky1@gmail.com>
2024-08-15 11:55:56 -0300

    tde feito 10 minatos

---
+++ Faculdade/Tde's/Tde 1/script.js
@@ -0,0 +1,85 @@
+// QUESTÃO 1
+// var a = +(prompt('Digite um número: '))
+// var b = +(prompt('Digite um número: '))
+// var c = +(prompt('Digite um número: '))
+// document.write(`${a}, ${b}, ${c}<br>`)
+// var letraA = (a*2)*(b/2)
+// var letraB = (a*3) + c
+// var letraC = c**3
+// document.write(`O produto do dobro do primeiro com metade do segundo é: ${letraA} <br>`)
+// document.write(`A soma do triplo do primeiro com o terceiro é: ${letraB}<br>`)
+// document.write(`O terceiro elevado ao cubo é: ${letraC}`)
+
+
+
+
+
+
+// QUESTÃO 2
+// var a = +(prompt('Digite um número: '))
+// document.write(`O número digitado foi: ${a}`)
+
+
+
+
+
+
+
+// QUESTÃO 3
+// var a = +prompt('Digite um valor em metros: ')
+// var cm = a*100
+// var mm = a*1000
+// document.write(`${a} em centímetos: ${cm}<br>${a} em milímetros: ${mm}`)
+
+
+
+
+
+
+
+
+//QUESTÃO 4
+// var a = +(prompt('Digite uma temperatura em Fahrenheit: '))
+// var c = 5*((a-32)/9)
+// document.write(`${a}F equivale a ${c.toFixed(1)} Celsius`)
+
+
+
+
+
+
+
+
+
+//QUESTÃO 5
+// var a = +(prompt('Digite um número para saber sua tabuada: '))
+// for(var i = 0; i <= 10; i++){
+//     document.write(`${i} * ${a} = ${i*a}<br>`)
+// }
+
+
+
+
+
+
+
+
+
+//QUESTÃO 6
+// var a = +prompt('Digite quanto você tem na carteira: ')
+// var d = a/5.40
+// document.write(`Com R$${a} você consegue comprar ${d.toFixed(1)}$.`)
+
+
+
+
+
+
+
+
+// QUESTÃO 7
+var l = +(prompt('Digite a largura: '))
+var h = +(prompt('Digite a altura: '))
+var area = l*h
+var tinta = area/2
+document.write(`Area = ${area}<br>Tinta necessária: ${tinta}`)
