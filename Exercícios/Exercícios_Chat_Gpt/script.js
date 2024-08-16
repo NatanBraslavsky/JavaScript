e1a91e89067236de5cfe14564d075e51b3daff14
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-16 00:00:46 -0300

    exercicio

--- Exercícios/Exercícios_Chat_Gpt/script.js
+++ Exercícios/Exercícios_Chat_Gpt/script.js
@@ -57,3 +57,65 @@
 //     return fat
 // }
 // document.write(`O fatorial de ${n} é ${fatorial(n)}`)
+
+
+
+
+
+
+
+// Função de Conversão de Celsius para Fahrenheit:
+// Crie uma função chamada celsiusParaFahrenheit que receba uma temperatura em Celsius e retorne a temperatura equivalente em Fahrenheit. A fórmula de conversão é: F = C * 9/5 + 32.
+
+// var grau = +(prompt('Digite um valor em graus'))
+// function celsiusFahr(x){
+//     let f = x * 9/5 + 32
+//     return f
+// }
+
+// document.write(`${grau}C convertido para Fahrenheit: ${celsiusFahr(grau)}`)
+
+
+
+
+
+
+
+// Função de Verificação de Palíndromo:
+// Escreva uma função chamada ePalindromo que receba uma string e retorne true se a string for um palíndromo (ou seja, se puder ser lida da mesma forma de trás para frente) e false caso contrário.
+
+// var str = prompt('Digite seu nome')
+// function palindromo(nome) {
+//     let string = nome.toLowerCase();
+//     let invert = string.split('').reverse().join('');
+//     return string === invert
+// }
+// document.write(palindromo(str)) 
+
+
+
+
+
+
+
+
+// Função de Maior Número em um Array:
+// Implemente uma função chamada maiorNumero que receba um array de números e retorne o maior número presente no array.
+
+let vetor = []
+
+for(let i = 0; i<5; i++){
+    let num = +(prompt(`Digite o ${i+1} número`))
+    vetor.push(num)
+}
+function maior(x){
+    let mai = 0
+    mai = vetor[0]
+    for(let i = 0; i < 5; i++){
+        if(x[i] > mai){
+            mai = x[i]
+        }
+    }
+    return mai
+}
+document.write(`Maior número no vetor: ${maior(vetor)}`)
