cdc4459eed7c4bca0838e8bee07cd8db075a8dd3
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-17 10:05:51 -0300

    exercício bondinho

---
+++ Exercícios/Exercícios_Neps_Academy/ex007(Bondinho)/script.js
@@ -0,0 +1,8 @@
+var a = +(prompt('Número de alunos:'))
+var m = +(prompt('Número de monitores'))
+var soma = a + m
+if(soma > 50){
+    alert('Capacidade ultrapassada.')
+} else {
+    alert('Capacidade disponível, boa viagem')
+}
