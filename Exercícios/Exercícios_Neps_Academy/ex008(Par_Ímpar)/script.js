38b902645420626f99635578e736ff163eea03c6
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-08-17 10:09:30 -0300

    exercício par ou impar

---
+++ Exercícios/Exercícios_Neps_Academy/ex008(Par_Ímpar)/script.js
@@ -0,0 +1,8 @@
+var b = +(prompt('Quantidade de dedos de Bino'))
+var c = +(prompt('Quantidade de dedos de Cino'))
+var soma = b + c
+if(soma % 2 == 0){
+    alert('Bino')
+} else {
+    alert('Cino')
+}
