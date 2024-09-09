b1ec6b6208340446578efe95d3212042deee117e
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-08 23:28:02 -0300

    mini projeto contador

---
+++ mini_projetos/contador/script.js
@@ -0,0 +1,13 @@
+var res = document.getElementById('res')
+
+function faumentar(){
+    res.innerHTML++
+}
+
+function fresetar(){
+    res.innerHTML = 0
+}
+
+function fdiminuir(){
+    res.innerHTML--
+}
