2c0d871612a6702a4333e7176063cefc153ffd19
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-10 16:19:40 -0300

    aula string js

---
+++ curso_web_moderno/fundamentos/funcoes/string.js
@@ -0,0 +1,14 @@
+const escola = "Cod3r champ"
+
+console.log(escola.charAt(4))//Caracter na posição x
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))//Existe x em y?
+
+console.log(escola.substring(2))//Exiba da posição 2 adiante
+console.log(escola.substring(0, 4))//exiba da posição 0 até o 4 e não mostre o 4
+
+console.log('Escola '.concat(escola).concat('!'))//concatenação
+console.log(escola.replace(3, 'e'))//Coloca 'e' no lugar do caracter 3.
+
+console.log('Ana,Maria,Pedro'.split(','))//Gera um array buscando pela ',' que pedi
+
