48de98669426768330611f35bf7e2aeaf9c710dd
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-12 20:21:38 -0300

    atualização da aula de hoje(peguei o da professora )

--- Faculdade/Aula/aula06/script.js
+++ Faculdade/Aula/aula06/script.js
@@ -1,96 +1,198 @@
-// var a = new Array(1,2)
-// document.write(a)
+//Metodo construtor() - new Array() -------------------------
+/*
+var a = new Array()
+document.write(`<br> ${a}`)
+document.write(`<br>O tamanho do array é ${a.length}`)
 
-// var b = new Array(true,1,2,3,4,new Array(1,2,3),'a')
-// document.write(`<br>${b}`)
-// document.write(`<br>${b[2]}`)
-// document.write(`<br>${b[5][1]}`)
-// document.write(`<br>${b[5][2]}`)
+var b = new Array(true,1,2,3,4,new Array(1,2,3),2,'a')
+document.write(`<br> ${b}`)
+document.write(`<br> ${b[2]}`)
+document.write(`<br> ${b[5][1]}`)
+document.write(`<br> ${b[5][2]}`)
 
-// //length
+//length - verifica o tamanho do array ------------------------
 
-// document.write(`<br>O tamanho do array é ${b.length}`)
+document.write(`<br>O tamanho do array é ${b.length}`)
 
-// //IndexOf
+//indexOf() - identifica a posição do elemento no array -------
 
-// document.write(`<br>A posição do elemento encontra-se na posição : ${b.indexOf('a')}`)
+document.write(`<br>A posição do elemento a encontra-se na posição : ${b.indexOf('a')}`)
 
-// //LastIndexOf()- informa a ultima ocorrência do elemento------
+//lastIndexOf() - informa a ultima ocorrência do elemento ------
 
-// document.write(`<br> A ultima ocorrência do elemento é: ${b.lastIndexOf(2)}`)
+document.write(`<br>A ultima occotência do elemento é: ${b.lastIndexOf(2)}`)
 
-// //Externo
+//Método literal - [] -----------------------------------------
 
-// var nota = []
+var c = []
+document.write(`<br> ${c}`)
+document.write(`<br>O tamanho do array é ${c.length}`)
 
-// for(let i = 0; i < 3; i++){
-//     nota[i] = +(prompt('Digite sua nota: '))
-// }
+var d = [true,1,2,3,4,[1,2,3],2,'a']
+document.write(`<br> ${d}`)
+document.write(`<br> ${d[0]}`)
+document.write(`<br> ${d[5][0]}`)
 
-// document.write(`<br>As notas são: ${nota}`)
+//valores externos -------------------------------------------
 
-// //Sting
-
-// nome = 'Thereza'
-// document.write(`<br>O tamanho do array é ${nome.length}`)
-// document.write(`<br> a posição do último elemento no array ${nome.lastIndexOf('e')}`)
-
-// //Inserir novos elementos no array
-
-// var e = [1,2,3,4]
-// e[4] = 'Novo valor'
-// document.write(`<br>${e}`)
-
-// //ou
-
-// e[e.length] = 'Valor Novo'
-// document.write(`<br>${e}`)
-
-
-
-// var pessoa = ['Ana', 'Liz', 'Eva', 'FLora', 'Isabel']
-
-// for(let i = 0; i < pessoa.length; i++){
-//     document.write(`<br>Nome: ${pessoa[i]}`)
-// }
-
-
-//Função de ordenação---------------
-
-// var pessoa = ['Ana', 'Liz', 'Eva', 'Flora', 'Isabel']
-// document.write(`<br>${pessoa}`)
-// document.write(`<br>A ordem invertida do array é : ${pessoa.reverse()}`)
-
-// //sort
-
-
-// document.write(`<br>A ordem alfabetica do array é ${pessoa.sort()}`)
-
-// //concat()
-
-// var f = [1,2,3,5]
-// var g = [6,7,8]
-// var concat = f.concat(g)
-// document.write(concat)
-// concat.shift()
-// document.write(`<br>${concat}`)
-
-// //includes() - verifica se um determinado elemento encontra-se no array
-
-// document.write(`<br>O numero 2 encontra-se no array? ${f.includes(2)}`) 
-
-
-
-// var semana = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo']
-
-// var add = semana.splice(1,2,'a','b')
-
-// document.write(`${semana} <br>- > removidos : ${add}`)
-
-var nome = []
-for(let i = 0; i < 5; i++){
-    nome[i] = prompt('Digite seu nome: ')
+nota =[]
+for(i=0; i<3;i++){
+	nota[i] = +(prompt('Digite a nota do aluno'))
 }
-document.write(`${nome}`)
-var n = nome.splice(2,0,'ana')
-document.write(`<br>${nome}`)
+document.write(`<br> As notas são: ${nota}`)
+
+//string ------------------------------------------------------
+
+nome = 'Thereza'
+document.write(`<br>O tamanho do array é ${nome.length}`)
+document.write(`<br>A posição do elemento no é array ${nome.lastIndexOf('e')}`)
+
+//inserir novos elementos no array -----------------------------
+
+var e = [1,2,3,4]
+e[4] = 'Novo valor'
+document.write(`<br>${e}`)
+
+//ou
+
+e[e.length]= 'Valor novo'
+document.write(`<br>${e}`)
+
+//---------------------------------------------------------------
+
+pessoa = ['Ana','Liz','Eva','Flora','Isabel']
+for(i=0;i<pessoa.length;i++){
+	document.write(`<br>Nome: ${pessoa[i]}`)
+}
+	
+//Função de ordenação------------------------------------------------
+reverse() - le o array de forma invertida -------------------------
+
+pessoa = ['Ana','Liz','Eva','Flora','Isabel']
+document.write(`<br>${pessoa}`)
+document.write(`<br>A ordem invertida do array é : ${pessoa.reverse()}`)
+
+//sort()- coloca em ordem alfabetica --------------------------------
+
+document.write(`<br>A ordem alfabetica do array é: ${pessoa.sort().reverse()}`)
+
+//concat() - concatenar vetores -------------------------------------
+
+var f = [1,2,3,5]
+var g = [6,7,8]
+
+document.write(`<br>O vetor concatenado é :${f.concat(g)}`)
+document.write(`<br>O vetor concatenado é :${g.concat(f)}`)
+
+//includes()- verifica se um determinado elemento encontra-se no array
+
+document.write(`<br>O numero 2 encontra-se no array? ${f.includes(2)}`)
+document.write(`<br>O numero 12 encontra-se no array? ${f.includes(12)}`)
+
+//push() - adiciona um novo elemento no final do array ---------------
+
+var h = [10,20,30,40]
+h.push(50)
+
+document.write(`<br>O novo array é: ${h}`)
+
+//pop() - remove o ultimo do elemento do array -------------------------
+
+h.pop()
+
+document.write(`<br>O novo array é: ${h}`)
+
+//unshift - adiciona um novo elemento no inicio do array ---------------
+
+h.unshift(0)
+
+document.write(`<br>O novo array é: ${h}`)
+
+//shift() - remove o primeiro do elemento do array -------------------------
+
+h.shift()
+
+document.write(`<br>O novo array é: ${h}`)
+
+//slice() - fatia o array sem alterar o array original ----------------
+
+aluno = 'Maria da Silva Xavier da Cunha'
+a = aluno.slice(5)
+document.write(`<br>${a}`)
+a1 = aluno.slice(5,14)
+document.write(`<br>${a1}`)
+
+carro = ['Jeta','Fiat','Focus','Cruze','Corolla','Civic','Elantra']
+c = carro.slice(2)
+document.write(`<br>${c}`)
+
+c1 = carro.slice(2,3)
+document.write(`<br>${c1}`)
+
+c2 = carro.slice(2,5)
+document.write(`<br>${c2}`)
+
+c3 = carro.slice(-4,-2)
+document.write(`<br>${c3}`)
+
+c4 = carro.slice(-6,-3)
+document.write(`<br>${c4}`)
+
+email = 'exemplo@gmail.com'
+e = email.slice(email.indexOf('@') + 1)
+document.write(`<br>${e}`)
+
+e1 = email.slice(email.lastIndexOf('e') + 1)
+document.write(`<br>${e1}`)
+
+e1 = email.slice(email.lastIndexOf('e'))
+document.write(`<br>${e1}`)
+
+nome1 = 'Thereza'
+n1 = nome1.slice(-3)
+document.write(`<br>${n1}`)*/
+
+/*splice() - 1º valor: posição
+			 2º valor: quantidade de elementos excluidos
+			 3º valor: novos elementos 
+			 
+d = ['Segunda','Terça','Quarta']
+d1 = d.splice(1,1)
+document.write(`<br>${d1}`)
+document.write(`<br>${d}`)
+
+d2 = d.splice(1,1,'Quinta','Sexta')
+document.write(`<br>${d2}`)
+document.write(`<br>${d}`)
+
+d3 = d.splice(1,0,'Quinta','Sexta')
+document.write(`<br>${d3}`)
+document.write(`<br>${d}`)
+
+nome = []
+for(i=0;i<5;i++){
+	nome[i] = prompt('Digite o seu nome')
+}
+document.write(`<br>Nome: ${nome}`)
+n = nome.splice(2,0,'1')
+document.write(`<br>Nome: ${nome}`)*/
+
+//Função anonima - não possui nome, atribui-se a função a uma variavel
+
+var msg = function(){
+	return `Olá!Bom dia!`
+}
+document.write(`<br>${msg()}`)
+//---------------------------------------
+var msg1 = function(){
+	return `Olá ${nome}!Bom dia!`
+}
+nome = prompt('Digite o seu nome')
+document.write(`<br>${msg1()}`)
+//---------------------------------------
+
+num = function(numero){
+	return numero ** 2
+}
+numero = +(prompt('Digite um numero'))
+document.write(`<br>${num(numero)}`)
