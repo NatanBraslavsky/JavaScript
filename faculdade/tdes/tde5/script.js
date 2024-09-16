e1c0c8325705447eed74c584c650cca53a32de89
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-16 18:44:49 -0300

    finalizei o tde de javascript

---
+++ faculdade/tdes/tde5/script.js
@@ -0,0 +1,70 @@
+//Número 1
+// var animais = ['cachorro', 'gato', 'pássaro']
+// animais.push('leão')
+// console.log(animais)
+
+
+//Número 2
+// var array = ['maça', 'banana', 'laranja']
+// var ultimo = array.pop()
+// console.log(ultimo)
+// console.log(array)
+
+
+//Número 3
+// var cores = ['vermelho', 'azul', 'verde']
+// cores.shift()
+// cores.unshift('roxo')
+// console.log(cores)
+
+
+//Número 4
+// let frutas = ['maçã', 'banana']
+// let vegetais = ['cenoura', 'batata']
+// let alimentos = frutas.concat(vegetais)
+// console.log(alimentos)
+
+
+//Número 5
+// let numeros = [1, 2, 3, 4, 5, 6]
+// let novo = numeros.slice(2, 5)
+// console.log(novo)
+
+
+//Número 6
+// let linguagens = ['JavaScript', 'Python', 'C++', 'Java']
+// let eliminar = linguagens.splice(1, 2, 'Ruby', 'Go')
+// console.log(linguagens)
+
+
+//Número 7
+// let numeros = [1, 2, 3, 4, 5]
+// let dobrado = numeros.map(function(numeros){
+//     return numeros * 2
+// }
+// )
+// console.log(dobrado)
+
+
+//Número 8
+// let idades = [10, 15, 20, 25, 30]
+// let filtro = idades.filter(function(idades){
+//     return idades >= 18
+// })
+// console.log(filtro)
+
+
+//Número 9
+// let precos = [10, 20, 30, 40]
+// let porcentagem = precos.map(function(precos){
+//     return precos * 1.10
+// })
+// console.log(porcentagem)
+
+
+//Número 10
+let numeros = [5, 12, 8, 130, 44]
+let filtro = numeros.filter(function(numeros){
+    return numeros > 10
+})
+console.log(filtro[0])
