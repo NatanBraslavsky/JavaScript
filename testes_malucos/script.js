280daa293d36d1743f8071ca63249b1683fc6af2
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-10-16 20:03:13 -0300

    revisão prova js

--- testes_malucos/script.js
+++ testes_malucos/script.js
@@ -1,8 +1,8 @@
-// var a = [2,4,6]
-// var pares = a.every( function(num){
-//     return num % 2 == 0
-// })
-// console.log(pares)
+var a = [2,4,6];
+var pares = a.every( function(num){
+    return num % 2 == 0;
+})
+console.log(pares);
 
 
 
@@ -23,3 +23,34 @@
 //         console.log('Quilometragem invalida.');
 // }
 // console.log(`Preco a pagar por ${quilometros}KM: R$${preco}`);
+
+
+// let peso = 70;
+// let altura = 1.70;
+// let imc = peso / Math.pow(altura,2);
+// console.log(`IMC: ${imc.toFixed(2)}`);
+
+
+
+// let anosFuma = +(prompt("Quantos anos fuma? "));
+// let cigarrosPorDia = +(prompt("Quantos cigarros por dia?"));
+// let precoCartela = +(prompt("Preço da cartela?"));
+// let totalPrecoGasto = (anosFuma * 365 * cigarrosPorDia / 20) * precoCartela;
+// alert("Voce gastou R$"+totalPrecoGasto);
+
+
+let morrer = function(vida, dano){
+    if(dano >= vida){
+        return true;
+    } else{
+        return false;
+    }
+}
+let dano = 99;
+let vida = 100;
+let morte = morrer(vida, dano);
+if(morte){
+    console.log('morreu');
+} else{
+    console.log('viveu');
+}
