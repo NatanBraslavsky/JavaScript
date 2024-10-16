b9379854b5fa28f5730df3f5bed82542458d31e6
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-10-16 17:42:16 -0300

    questão revisão JS

--- testes_malucos/script.js
+++ testes_malucos/script.js
@@ -1,5 +1,25 @@
-var a = [2,4,6]
-var pares = a.every( function(num){
-    return num % 2 == 0
-})
-console.log(pares)
+// var a = [2,4,6]
+// var pares = a.every( function(num){
+//     return num % 2 == 0
+// })
+// console.log(pares)
+
+
+
+// let quilometros = +(prompt("Digite a quilometragem: "));
+// let quilometros = 301;
+// let preco = 0;
+// switch(true){
+//     case (quilometros <= 100):
+//         preco = quilometros * 10;
+//         break;
+//     case (quilometros <= 300):
+//         preco = quilometros * 8;
+//         break;
+//     case (quilometros > 300):
+//         preco = quilometros * 6;
+//         break;
+//     default:
+//         console.log('Quilometragem invalida.');
+// }
+// console.log(`Preco a pagar por ${quilometros}KM: R$${preco}`);
