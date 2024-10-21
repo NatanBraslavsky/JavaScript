1a3000d5e07aed58baf72b5cd24e75dd169b85ca
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-10-21 19:57:42 -0300

    questão da prova de noite

---
+++ testes_malucos/prova_noite_teste/script.js
@@ -0,0 +1,24 @@
+// function newton(){
+//     let massa = +(prompt('Massa: '))
+//     let aceleracao = +(prompt('Aceleração: '))
+//     let result = massa * aceleracao
+//     alert(result)
+// }
+// function cinetica(){
+//     let massa = +(prompt('Massa: '))
+//     let velocidade = +(prompt('Velocidade: '))
+//     let result = 0.5 * massa * Math.pow(velocidade, 2) 
+//     alert(result)
+// }
+// let calculo = +(prompt('Digite qual calculo deseja. 1- newton, 2- cinética: '))
+// if(calculo == 1){
+//     newton()
+// } else if(calculo == 2){
+//     cinetica()
+// } else{
+//     alert('Valor Inválido.')
+// }
+
+
+
+
