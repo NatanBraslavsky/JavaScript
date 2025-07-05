29266cacaa8e5cba5c5450fa0871dac998c9b67c
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-05 19:58:39 -0300

    ordenar array js

--- testes_malucos/script.js
+++ testes_malucos/script.js
@@ -185,12 +185,30 @@
 // Função de Fatorial:
 // Implemente uma função chamada fatorial que receba um número inteiro n e retorne o fatorial de n (n!).
 
-var n = +(prompt('Digite um número'))
-function fatorial(x) {
-    let fat = 1
-    for(var i = x; i > 0; i--){
-        fat *= i
+// var n = +(prompt('Digite um número'))
+// function fatorial(x) {
+//     let fat = 1
+//     for(var i = x; i > 0; i--){
+//         fat *= i
+//     }
+//     return fat
+// }
+// document.write(`O fatorial de ${n} é ${fatorial(n)}`)
+
+
+ function ordenar(array){
+    let temp = 0
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[i] < array[j]){
+                temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
     }
-    return fat
+    return array
 }
-document.write(`O fatorial de ${n} é ${fatorial(n)}`)
+let array = [7,5,6,4,5,3,8,5,22]
+array = ordenar(array)
+console.log(array)
