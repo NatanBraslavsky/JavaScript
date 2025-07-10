a83af45e7678eaf440585244aa81491b99cb3f17
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-10 15:55:56 -0300

    pedra papel e tesoura teste logica

--- curso_yt/rock-paper-scissors/script.js
+++ curso_yt/rock-paper-scissors/script.js
@@ -1,70 +1,21 @@
-let btnChoice = window.document.querySelectorAll(".btnChoice")
+function play(escolhaUsuario){
+    const opcoes = ["Rock", "Paper", "Scissors"]
+    const escolhaPc = opcoes[Math.floor(Math.random() * 3)]
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    let resultado = ''
+
+    if(escolhaUsuario == escolhaPc){
+        resultado = 'Empate!'
+    } else if(
+        (escolhaUsuario === 'Rock' && escolhaPc === 'Scissors') ||
+        (escolhaUsuario === 'Paper' && escolhaPc === 'Rock') ||
+        (escolhaUsuario === 'Scissors' && escolhaPc === 'Paper')
+    ) {
+        resultado = 'Você Ganhou!'
+    } else{
+        resultado = 'Você Perdeu!'
+    }
+
+    alert(`Você escolheu ${escolhaUsuario} e o Pc escolheu ${escolhaPc}, logo ${resultado}`)
 }
 
-let pcChoice = getRandomInt(3)
-let escolhaPc = {
-    0:"Rock",
-    1:"Paper",
-    2:"Scissors",
-}
-
-btnChoice.forEach(btn => {
-    btn.addEventListener("click", ()=> {
-        if(btn.getAttribute('value')==0){
-            alert(`Your choice: 'Rock'. Computer choice: ${escolhaPc[pcChoice]}`)
-            if(pcChoice == 0){
-                alert("Draw.")
-            } else if(pcChoice == 1){
-                alert("Pc wins.")
-            } else{
-                alert("You win.")
-            }
-        } else if(btn.getAttribute('value')==1){
-            alert(`Your choice: 'Paper'. Computer choice: ${escolhaPc[pcChoice]}`)
-            if(pcChoice == 0){
-                alert("You win")
-            } else if(pcChoice == 1){
-                alert("Draw")
-            } else{
-                alert("Pc wins")
-            }
-        } else{
-            alert(`Your choice: 'Scissors'. Computer choice: ${escolhaPc[pcChoice]}`)
-            if(pcChoice == 0){
-                alert("Pc wins")
-            } else if(pcChoice == 1){
-                alert("You win")
-            } else{
-                alert("Draw")
-            }
-        }
-    })
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const escolha = {
-//     "rock":0,
-//     "paper":1,
-//     "scissors":2,
-// }
