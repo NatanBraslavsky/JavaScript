ebe37662b3a3460e0173093cd3861b847b20e280
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-12 11:49:45 -0300

    javascript revisão

---
+++ curso_yt/fundamentos/advanced_functions/script.js
@@ -0,0 +1,9 @@
+//? dois parametros(função, tempo)
+//!executa a função uma vez em 3 segundos
+setTimeout(() =>{
+    console.log('settimeout')
+}, 3000)
+
+setInterval(() =>{
+    console.log('setinterval')
+}, 1000)
