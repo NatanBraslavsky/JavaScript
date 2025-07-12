215531252f9827eca8bf1af83d7ec20eefb8535b
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-12 11:57:37 -0300

    testes funções

--- curso_yt/fundamentos/advanced_functions/script.js
+++ curso_yt/fundamentos/advanced_functions/script.js
@@ -4,6 +4,10 @@ setTimeout(() =>{
     console.log('settimeout')
 }, 3000)
 
+//!executa a função repetidamente em um intervalo de tempo
 setInterval(() =>{
     console.log('setinterval')
 }, 1000)
+
+window.document.querySelectorAll('button')
+.forEach()
