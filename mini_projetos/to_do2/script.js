2ab7ef2428a0f21491212291f19ba4c842b32776
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-18 07:46:50 -0300

    to do 2 revisao

---
+++ mini_projetos/to_do2/script.js
@@ -0,0 +1,28 @@
+document.querySelector('#btnAdd').addEventListener('click', ()=>{
+    if(document.querySelector('#inputAdd').value.length == 0){
+        alert('Campo obrigatório')
+    } else{
+        const inputText = document.querySelector('#inputAdd').value
+        document.querySelector('.containerPrincipalTarefas').innerHTML += `
+            <div class="containerTarefas">
+                <div class="tarefas">${inputText}</div>
+                <button class="delete">X</button>
+            </div>
+        `
+        const btnDelete = document.querySelectorAll(".delete")
+        for(let i = 0; i < btnDelete.length; i++){
+            btnDelete[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        const contTarefas = document.querySelectorAll('.containerTarefas')
+        for(let i = 0; i < contTarefas.length; i++){
+            contTarefas[i].onclick = function(){
+                const task = document.querySelectorAll('.tarefas');task[i].classList.toggle('feito')
+            }
+        }
+    }
+});
+
+
