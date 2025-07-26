5f341199d2daa0b3be8be34ec376d7f7e7d99d25
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-26 20:48:10 -0300

    revisao fatorial

--- exercicios/exercicios_chat_gpt/ex000_juncao/script.js
+++ exercicios/exercicios_chat_gpt/ex000_juncao/script.js
@@ -102,20 +102,31 @@
 // Função de Maior Número em um Array:
 // Implemente uma função chamada maiorNumero que receba um array de números e retorne o maior número presente no array.
 
-let vetor = []
+// let vetor = []
 
-for(let i = 0; i<5; i++){
-    let num = +(prompt(`Digite o ${i+1} número`))
-    vetor.push(num)
-}
-function maior(x){
-    let mai = 0
-    mai = vetor[0]
-    for(let i = 0; i < 5; i++){
-        if(x[i] > mai){
-            mai = x[i]
-        }
+// for(let i = 0; i<5; i++){
+//     let num = +(prompt(`Digite o ${i+1} número`))
+//     vetor.push(num)
+// }
+// function maior(x){
+//     let mai = 0
+//     mai = vetor[0]
+//     for(let i = 0; i < 5; i++){
+//         if(x[i] > mai){
+//             mai = x[i]
+//         }
+//     }
+//     return mai
+// }
+// document.write(`Maior número no vetor: ${maior(vetor)}`)
+
+
+var n = +(prompt('Digite um número'))
+function fatorial(x) {
+    let fat = 1
+    for(var i = x; i > 0; i--){
+        fat *= i
     }
-    return mai
+    return fat
 }
-document.write(`Maior número no vetor: ${maior(vetor)}`)
+document.write(`O fatorial de ${n} é ${fatorial(n)}`)
