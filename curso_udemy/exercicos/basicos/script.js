a638a8aba328e9ba286c9e953382905aa21b5fba
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-09-09 12:18:34 -0300

    exercicio logica prog

--- curso_udemy/exercicos/basicos/script.js
+++ curso_udemy/exercicos/basicos/script.js
@@ -10,3 +10,21 @@ document.querySelector('button').addEventListener('click', ()=>{
         `${pes.name}, ${pes.idade}, ${pes.sexo}`
     )
 });
+
+//? exercício for
+
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
+]
+
+const containerElementos = document.querySelector("#exFor");
+elementos.map((element) => {
+    const ele = document.createElement(`${element.tag}`);
+    ele.textContent = element.texto;
+    containerElementos.appendChild(ele);
+})
+
+
